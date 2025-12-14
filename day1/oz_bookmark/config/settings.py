8765662491468130268553563615eafbd0c296d2
god-gy <god.gy0321@gmail.com>
2025-12-14 18:46:17 +0900

    Setting .env

--- day1/oz_bookmark/config/settings.py
+++ day1/oz_bookmark/config/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52-aagy**o=s@jww8+sh6-o38&_lm$8$)m5*#@$++bgako3l&!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
