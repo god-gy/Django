f40c4e159784dab35bbf6c4938b25aa5b2a4fd19
god-gy <god.gy0321@gmail.com>
2025-12-15 19:52:46 +0900

    ORM 활용

--- day1/bookmark/bookmark/views.py
+++ day1/bookmark/bookmark/views.py
@@ -5,7 +5,11 @@ from bookmark.models import Bookmark
 
 
 def bookmark_list(request):
-    bookmarks = Bookmark.objects.all()
+    # 전체 리스트 보여주기
+    # bookmarks = Bookmark.objects.all()
+
+    # id가 50 이상인것만 보여주기
+    bookmarks = Bookmark.objects.filter(id__gte=50)
 
     context = {'bookmarks': bookmarks}
     return render(request, 'bookmark_list.html', context)
