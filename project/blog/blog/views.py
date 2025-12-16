4b1704a40bbaee5f666c30193a1a78236d83813e
god-gy <god.gy0321@gmail.com>
2025-12-16 11:39:31 +0900

    Blog project
    방문자 수 쿠키 생성

--- project/blog/blog/views.py
+++ project/blog/blog/views.py
@@ -5,9 +5,15 @@ from blog.models import Blog
 
 def blog_list(request):
     blogs = Blog.objects.all()
+
+    visits = int(request.COOKIES.get('visits', 0)) +1
+
     context = {'blogs': blogs}
 
-    return render(request, 'blog_list.html', context)
+    response = render(request, 'blog_list.html', context)
+    response.set_cookie('visits', visits)
+
+    return response
 
 def blog_detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
