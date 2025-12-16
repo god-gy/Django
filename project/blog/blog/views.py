9e413744de5460368d7ab4c3507e388c0dcc25e5
god-gy <god.gy0321@gmail.com>
2025-12-16 11:42:43 +0900

    Blog project
    세션 생성 및 front단에서 확인

--- project/blog/blog/views.py
+++ project/blog/blog/views.py
@@ -8,7 +8,12 @@ def blog_list(request):
 
     visits = int(request.COOKIES.get('visits', 0)) +1
 
-    context = {'blogs': blogs}
+    request.session['count'] = request.session.get('count', 0) + 1
+
+    context = {
+        'blogs': blogs,
+        'count': request.session['count'],
+    }
 
     response = render(request, 'blog_list.html', context)
     response.set_cookie('visits', visits)
