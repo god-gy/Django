4292306f3bcabdbe918f29cf016d249207ff78c0
god-gy <god.gy0321@gmail.com>
2025-12-18 17:13:58 +0900

    ✨feat: chapter_04 과제 업로드
    TodoUpdate Form 분리 및 Field 추가

--- task/todolist/todolist/form.py
+++ task/todolist/todolist/form.py
@@ -6,3 +6,8 @@ class TodolistForm(forms.ModelForm):
     class Meta:
         model = ToDoList
         fields = ("title", "description", "start_date", "end_date")
+
+class TodoUpdateForm(forms.ModelForm):
+    class Meta:
+        model = ToDoList
+        fields = ("title", "description", "start_date", "end_date", "is_complete")
