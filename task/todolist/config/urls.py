bd88f87506903db81fb4177181b874682ee39ee4
god-gy <god.gy0321@gmail.com>
2025-12-18 20:46:56 +0900

    ✨feat: chapter_05 과제 업로드
    CBV 적용, URL include 적용하기, admin 권한 사용하기

--- task/todolist/config/urls.py
+++ task/todolist/config/urls.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from todolist import views
 from member import views as member_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.todo_list, name='todolist'),
-    path('<int:pk>/', views.todo_info, name='todoinfo'),
+
+    # FBV urls
+    # path('', views.todo_list, name='todolist'),
+    # path('<int:pk>/', views.todo_info, name='todoinfo'),
+    # path('create/', views.todo_create, name='todo_create'),
+    # path('<int:pk>/update/', views.todo_update, name='todo_update'),
+    # path('<int:pk>/delete/', views.todo_delete, name='todo_delete'),
+
+    # USER urls
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', member_views.sign_up, name='signup'),
-    path('create/', views.todo_create, name='todo_create'),
-    path('<int:pk>/update/', views.todo_update, name='todo_update'),
-    path('<int:pk>/delete/', views.todo_delete, name='todo_delete'),
+
+    # CBV urls
+    path('todo/', include('todolist.cbv_urls')),
 ]
