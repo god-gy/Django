aaec4a557de69ea504488fc9bf3387f3505a9e51
god-gy <god.gy0321@gmail.com>
2025-12-27 14:03:24 +0900

    Pystagram project
    이메일 인증기능 생성

--- project/pystagram/member/views.py
+++ project/pystagram/member/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponseRedirect
+from django.core import signing
+from django.core.signing import TimestampSigner
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView
@@ -13,6 +14,21 @@ class SignUpView(FormView):
 
     def form_valid(self, form):
         user = form.save()
+        # 이메일 발송
+        signer = TimestampSigner()
+        signed_user_email = signer.sign(user.email)
+        signer_dump = signing.dumps(signed_user_email)
+        # print(signer_dump)
+
+        # 암호화 해제 방법
+        # decoded_user_email = signing.loads(signer_dump)
+        # print(decoded_user_email)
+        # email = signer.unsign(decoded_user_email, max_age=60*30)
+        # print(email)
+
+        url = f'{self.request.scheme}://{self.request.META["HTTP_HOST"]}/verify/?code={signer_dump}'
+        # print(url)
+
         return render(
             self.request,
             template_name='auth/signup_done.html',
